aba03cd3ea5f8686e50a4f9730f04ea0d7fc6062
Ana-Maria Piso <anamaria.piso@gmail.com>
2017-05-08 17:59:34 -0700

    upd

--- python/evaporation_2.py
+++ python/evaporation_2.py
@@ -13,7 +13,7 @@ from scipy import integrate, interpolate, optimize
 from scipy.integrate import odeint
 from types import FunctionType as function
 from scipy.interpolate import interp1d
-from scipy.optimize import brentq
+from scipy.optimize import brentq, root
 from profiles_SG import atmload
 from profiles_no_SG import atmload as atmloadnoSG
 from cooling import cooling_global
@@ -205,35 +205,97 @@ def mass_loss_2(filename, prms, td = 3e6, n = 500, tol = 1e-24, SG = 1):
     rf = r[indf] / model.rco[0]
     t = -(EtotB - Etot[indf])/L
     
-    return L, t, Mf, rf, MBd
+    return L, t, Mf, rf, MBd, r, P, T, rho, m, delrad, Eg, U, Mcb, MB, rcb, RB, Pc, Pcb, Tc, Tcb, \
+            Egcb, Ucb, Etotcb, EgB, UB, EtotB, Etot, Ecool, Eevap, indf
     
     
+    
+    
+def matching(filename, prms, td = 3e6, n = 500, tol = 1e-24, SG = 1):
+    
+    L, t, Mf, rf, MBd, r, P, T, rho, m, delrad, Eg, U, Mcb, MB, rcb, RB, Pc, Pcb, Tc, Tcb, \
+            Egcb, Ucb, Etotcb, EgB, UB, EtotB, Etot, Ecool, Eevap, indf =  mass_loss_2(filename, prms, td, n, tol, SG)
+            
+    sol = shoot(MBd * Me, L*1e-1, L*1e1, n, tol, prms)
+    r, P, T, m, rho, delrad, Eg, U, Mi, Mcb, MB, rcb, \
+            RB, rfit, Pc, Pcb, PB, Tc, Tcb, TB, Egcb, Ucb, Etotcb, \
+                EgB, UB, EtotB, EgHill, UHill, EtotHill, L, vircb, virHill, err = sol     
+    Etot = Eg + U
+    
+    Evap = Etot[-1] - Etot[-2]
+    dt = - Eevap / L
+    Ecool = Etot[-2]
     
 #    
-#    
-#    def f(x, r):
-#    #    
-#    #"""
-#    #structure eqns. "x" = [p , T , m, L, Eg, U, Iu],
-#    #    with Iu the 3p/rho dm integral in the virial theorem
-#    #dp/dr = - G * m * P / (r**2 * R * T),
-#    #dT/dr = - del * G * m / (R * r**2)
-#    ###dm/dr = 4 * pi * r**2 * p / (R * T)
-#    #dL/dr = 0
-#    ###dEg/dr = - 4 * pi * G * m * r * P / (R * T)
-#    #dU/dr = 4 * pi * r**2 * P * Cv / R
-#    #"""
-#        
-#        return np.array([ - G * x[2] * x[0] / (r**2 * prms.R * x[1]), \
-#                             - Del(x[0], x[2], x[1], x[3], prms) * G * x[2] / \
-#                             (prms.R * r**2),
-#                             4 * pi * r**2 * x[0] / (prms.R * x[1]), \
-#                             0, \
-#                             - 4 * pi * G * x[2] * r * x[0] / (prms.R * x[1]), \
-#                             4 * pi * r**2 * x[0] * prms.Cv / prms.R]) 
-#    #E0 = G * Mi**2 / rfit
-#    R = np.logspace(np.log10(RBd*Re), np.log10(model.rco), nMpoints)
-#        #radius grid
+    
+    def f(x, r):
+    #    
+    #"""
+    #structure eqns. "x" = [p , T , m, L, Eg, U, Iu],
+    #    with Iu the 3p/rho dm integral in the virial theorem
+    #dp/dr = - G * m * P / (r**2 * R * T),
+    #dT/dr = - del * G * m / (R * r**2)
+    ###dm/dr = 4 * pi * r**2 * p / (R * T)
+    #dL/dr = 0
+    ###dEg/dr = - 4 * pi * G * m * r * P / (R * T)
+    #dU/dr = 4 * pi * r**2 * P * Cv / R
+    #"""
+        
+        return np.array([ - G * x[2] * x[0] / (r**2 * prms.R * x[1]), \
+                             - Del(x[0], x[2], x[1], x[3], prms) * G * x[2] / \
+                             (prms.R * r**2),
+                             4 * pi * r**2 * x[0] / (prms.R * x[1]), \
+                             0, \
+                             - 4 * pi * G * x[2] * r * x[0] / (prms.R * x[1]), \
+                             4 * pi * r**2 * x[0] * prms.Cv / prms.R]) 
+    E0 = 0#G * prms.Mco**2 / prms.rco
+    R = np.logspace(np.log10(prms.rco), np.log10(r[-2]), n)
+    
+    y = odeint(f, [Pc, Tc, prms.Mco, L, -E0, E0], [prms.rco, r[-2]])
+     
+    def deltaM(tcore):     
+           
+        y = odeint(f, [Pc, tcore, prms.Mco, L, -E0, E0], [prms.rco, r[-2]])                     
+        Mtot = y[:,2][1]
+        deltaM = 4 / np.pi * np.arctan(Mtot / m[-2]) - 1
+        #relative error; use of the arctan ensures deltaL stays between -1 and 1
+        if math.isnan(deltaM): #used to get rid of possible divergences
+                deltaM = 1.
+        return deltaM
+        
+        
+    def delta(x):  
+           
+        #tcore = x[0]
+        #Pcore = x[1]
+        #lum = x[2]   
+           
+        y1 = odeint(f, [x[1], x[0], prms.Mco, x[2], -E0, E0], [prms.rco, r[-2]])                     
+        Mtot = y1[:,2][1]
+        deltaM = 4 / np.pi * np.arctan(Mtot / m[-2]) - 1
+        #relative error; use of the arctan ensures deltaL stays between -1 and 1
+        if math.isnan(deltaM): #used to get rid of possible divergences
+                deltaM = 1.
+                
+        #deltaM = 4 / np.pi * np.arctan(Mtot / m[-2]) - 1
+        ##relative error; use of the arctan ensures deltaL stays between -1 and 1
+        #if math.isnan(deltaM): #used to get rid of possible divergences
+        #        deltaM = 1.
+        return deltaM
+
+        
+        
+    tcoreguess = 0.9 * Tc
+    Pcoreguess = Pc/ 10   
+    Lguess = L*1e-1
+
+    Tmatch = root(deltaM, 0.9*Tc)
+    #Tmatch = brentq(delta, T1, T2, xtol = tol)
+    
+    
+    
+    
+        #radius grid
 #    r = R[:2]    
 #    
 #    y = odeint(f, [prms.Pd, prms.Td, MBd*Me, Ld, EgBd, UBd], r)
